576caf3ccb3dcf1e6124737cbf836c90a95ec79d
YangDong <42331224+Yang2002@users.noreply.github.com>
2018-09-24 12:09:12 +0800

    Some small changes to improve execution time
     - A faster way to calculate log2(x)
     - When dep[x] > dep[y], use log2 function to speed up the jump

--- LCA(Doubling).cpp
+++ LCA(Doubling).cpp
@@ -1,86 +1,76 @@
-// using doubling algorithm to get the LCA of any two nodes in a tree
-// Complexity: O(nlogn) precalculate and O(logn) for each query
-
+// O(nlogn) Precalculate and O(log n) per query
+// LCA::solve() is to test the algorithm
+// Input format:
+// n, q, root
+// n-1 lines describes edges x <-> y
+// q lines describes queries lca(x, y)
 #include<bits/stdc++.h>
-#define REP(i,n) for(int i = 0; i < n; ++i)
-#define rep(i,n) for(int i = 1; i <= n; ++i)
-#define boost ios::sync_with_stdio(0)
-#define ms0(s) memset((s), 0, sizeof(s))
-#define INF 0x3f3f3f3f
-#define ABS(x) ((x)<0 ? (-(x)) : (x))
-#define forvec(it,v) for(vector<int>::iterator it = v.begin(); it != v.end(); ++it)
-
+#define rep(i, n) for(int i = 1; i <= (n); ++i)
 using namespace std;
 typedef long long ll;
-template <typename T>
-inline void read(T& x){
-    int w = 1; T data = 0;
-    char ch = getchar();
-    while(!isdigit(ch) && ch != '-') ch = getchar();
-    if(ch == '-') w = -1, ch = getchar();
-    while(isdigit(ch))
-        data = 10 * data + ch - '0', ch = getchar();
-    x = data * w;
-}
-template <typename T>
-void write(T x){
-	 if (x < 0) putchar('-'),x = ~x + 1;
-	 if (x > 9) write(x / 10);
-	 putchar( x % 10 + '0');
-}
-const int maxn = 500020;
-int anc[maxn][30], lg[maxn], dep[maxn], n, m, s;
-int head[maxn], k = 0;
-struct node{
-    int v,next;
-}e[maxn*2];
-void add(int u,int v)
-{
-    e[k].v = v;
-    e[k].next = head[u];
-    head[u] = k++;
-}
-inline void init_log(){
-    for(int i = 1; i <= n; ++i)
-        lg[i] = lg[i - 1] + (1 << (lg[i-1]+1) == i);
-}
-void dfs(int v, int depth){
-    dep[v] = depth;
-    for(int i = head[v], u = e[i].v ; i != -1; i = e[i].next, u = e[i].v ) if(!dep[u]){
-        anc[u][0] = v;
-        dfs(u, depth + 1);
+const int maxn = 500020, maxm = 1000020;
+int anc[maxn][30], root, dep[maxn], lg[maxn];
+namespace adjlist{
+    int n, m, ecnt = 0, head[maxn], nxt[maxm];
+    struct edge{
+        int from, to;
+        edge(int x = 0, int y = 0) : from(x), to(y) {}
+    } e[maxm];
+    inline void init(int sz){
+        memset(head, 0, sizeof(int) * (sz + 10));
+        ecnt = 0;
     }
-}
-inline void lca_init(){
-    for(int j = 1; j <= lg[n]; ++j)
-        for(int i = 1; i <= n; ++i)
-            anc[i][j] = anc[anc[i][j-1]][j-1];
-}
-inline int LCA(int x, int y){
-    if(dep[x] < dep[y]) swap(x, y);
-    while(dep[x] > dep[y]) x = anc[x][lg[dep[x] - dep[y]]];
-    if(x == y) return x;
-    for(int k = lg[dep[x]]; k >= 0; --k){
-        if(anc[x][k] != anc[y][k])
-            x = anc[x][k], y = anc[y][k];
+    inline void addedge(int x, int y){
+        e[++ecnt] = edge(x, y);
+        nxt[ecnt] = head[x];
+        head[x] = ecnt;
     }
-    return anc[x][0];
-}
-int main()
-{
-    memset(head, -1, sizeof head);
-    read(n), read(m), read(s);
-    rep(i, n-1){
-        int p, q; read(p), read(q);
-        add(p, q), add(q, p);
-    }
-    init_log();
-    dfs(s, 1);
-    lca_init();
-    rep(i, m){
-        int p, q; read(p), read(q); 
-        write(LCA(p, q)), putchar('\n');
-    }
-    return 0;
 }
 
+namespace LCA{
+    using namespace adjlist;
+    int LOG;                                                                           
+    void dfs(int v, int fa, int depth){
+        anc[v][0] = fa, dep[v] = depth;
+        for(int i = 1; i <= LOG; ++i)
+            anc[v][i] = anc[ anc[v][i-1] ][i-1];
+        for(register int i = head[v] ; i ; i = nxt[i]) {
+            if(e[i].to != fa) dfs(e[i].to, v, depth + 1);
+        }
+    }
+    inline int LCA(int x, int y){
+        if(dep[x] < dep[y]) swap(x, y);
+        while(dep[x] > dep[y]) x = anc[x][ lg[dep[x] - dep[y]] ];
+        if(x == y) return x;
+        for(int i = lg[dep[x]]; i >= 0; --i) if(anc[x][i] != anc[y][i]) 
+            x = anc[x][i], y = anc[y][i];
+        return anc[x][0];
+    }
+    inline void init(int root){
+        LOG = lg[n] + 1;
+        dfs(root, root, 1);
+    }
+    inline void solve(){
+        int rt, q;
+        scanf("%d%d%d", &n, &q, &rt);
+        adjlist::init(n);
+        m = (n - 1) << 1;
+        rep(i, n - 1) {
+            int x, y; scanf("%d%d", &x, &y); 
+            addedge(x, y), addedge(y, x);
+        }
+        init(rt);
+        while(q--){
+            int x, y; scanf("%d%d", &x, &y);
+            printf("%d\n", LCA(x, y) );
+        }
+    }
+}
+inline void log_init(){
+    for(register int i = 2; i <= 500000; ++i)
+        lg[i] = lg[i - 1] + (1 << (lg[i-1] + 1) == i);
+}
+int main(){
+    log_init();
+    LCA::solve();
+}
